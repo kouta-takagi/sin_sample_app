21b0ee37616f82332bda129cc06a8c7865512545
kouta-takagi <k-takagi@ruby-dev.jp>
2024-10-01 16:30:57 +0900

    終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
   def new
     @user = User.new
   end
@@ -19,15 +22,46 @@ class UsersController < ApplicationController
   end
 
   def edit
-
   end
 
   def update
+    if @user.update(user_params)
+      flash[:success] = "でけた"
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
 
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url, status: :see_other
   end
 
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "ログインせーよ"
+      redirect_to login_url, status: :see_other
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url, status: :see_other) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url, status: :see_other) unless current_user.admin?
   end
 end
